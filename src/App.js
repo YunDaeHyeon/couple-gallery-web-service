bcb26caf0ff67dcbc4ec1257f08ca4909bfae255
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-01-26 14:44:15 +0000

    23.01.26 NotFoundPage Routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import FirstPage from './route/FirstPage';
 import LoginPage from './route/LoginPage';
 import RegisterPage from './route/RegisterPage';
+import NotFound from './components/NotFound';
 import './App_style.css';
 import { AuthProvider } from './authentiaction/auth';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/home" element={<FirstPage />}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
   );
 }
